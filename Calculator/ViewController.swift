0b84e95ceab0b12616d7042d1e593bb393397bb1
Henry Stanley <henry@henrystanley.com>
2015-04-12 14:49:25 +0100

    updated viewcontroller to perform basic math operations

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -18,7 +18,7 @@ class ViewController: UIViewController {
     // automatically for you.
     @IBOutlet weak var display: UILabel!
     
-    var userIsInTheMiddleOfTypingANumber: Bool = false
+    var userIsInTheMiddleOfTypingANumber = false
 
     
     // okay, this is a method (a 'func'tion). Its type is UIButton. 
@@ -36,5 +36,62 @@ class ViewController: UIViewController {
             userIsInTheMiddleOfTypingANumber = true
         }
     }
+    
+    
+    @IBAction func operate(sender: UIButton) {
+        let operation = sender.currentTitle!
+        if userIsInTheMiddleOfTypingANumber {
+            enter()
+        }
+        switch operation {
+            // here, we're calling performOperation but passing in a function as an argument - 
+            // a *closure*! performOperation pops off the right numbers from our stack
+            // and then executes the function we've passed to it, rendering that output
+            // on the display.
+            case "+": performOperation() { $0 + $1 }
+            case "–": performOperation() { $1 - $0 }
+            case "÷": performOperation() { $1 / $0 }
+            case "×": performOperation() { $0 * $1 }
+            case "√": performOperation() { sqrt($0) }
+            default: break
+        }
+    }
+    
+    // here, we're defining a function that takes in two doubles and returns a double.
+    func performOperation(operation: (Double, Double) -> Double) {
+        if operandStack.count >= 2 {
+            displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
+            enter()
+        }
+    }
+    
+    // here, we're redefining performOperation - just with different arguments. weird.
+    func performOperation(operation: Double -> Double) {
+        if operandStack.count >= 1 {
+            displayValue = operation(operandStack.removeLast())
+            enter()
+        }
+    }
+    
+    var operandStack = Array<Double>()
+    @IBAction func enter() {
+        // this gets the numbers from the display and adds them to a stack.
+        userIsInTheMiddleOfTypingANumber = false
+        operandStack.append(displayValue)
+        println(operandStack)
+    }
+    
+    var displayValue: Double {
+        // this guy gets and sets the value of the display and does
+        // type conversion to switch it between String and Double
+        get {
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set {
+            display.text = "\(newValue)"
+            userIsInTheMiddleOfTypingANumber = false
+        }
+    }
+    
 }
 
